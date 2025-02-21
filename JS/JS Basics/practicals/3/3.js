db06df7c53ad775ebf0abb9517f4a11345b78d0d
Mshkss <tolya200059@gmail.com>
2025-02-21 18:27:25 +0700

    3.js

--- JS/JS Basics/practicals/3/3.js
+++ JS/JS Basics/practicals/3/3.js
@@ -1,1 +1,15 @@
-alert( 'Привет, мир!' );
+//alert( 'Привет, мир!' );
+let arr =[
+	{name:"apple", count: 5, price: 70},
+	{name:"orange", count: 10, price: 90}
+];
+
+let sum = 0;
+
+
+for(let i = 0; i < arr.length; i++){
+sum+=arr[i].count * arr[i].price;
+}
+
+
+console.log("price = ", sum)
