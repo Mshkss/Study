9bd726e64e788945ae248f70fe33dba573259b98
Mshkss <tolya200059@gmail.com>
2025-04-24 21:57:40 +0700

    task7js added some pages

--- JS/JS Basics/practicals/7(node.js,express)/task7.js
+++ JS/JS Basics/practicals/7(node.js,express)/task7.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
 const app = express();
+const mustacheExpress = require("mustache-express");
 const PORT = 3000;
 
+// Устанавливаем mustache как шаблонизатор
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+//
+
+const homeData = require("./data/home.json");
+
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.render("home", homeData);
 });
 
 app.listen(PORT, () => {
