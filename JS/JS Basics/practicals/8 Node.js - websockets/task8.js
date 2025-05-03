8b67fc5d93b6029c8b8a9fe942079162b711367b
Mshkss <tolya200059@gmail.com>
2025-05-03 15:06:14 +0700

    task8 almost did

--- JS/JS Basics/practicals/8 Node.js - websockets/task8.js
+++ JS/JS Basics/practicals/8 Node.js - websockets/task8.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
-// app.use(express.urlencoded({ extended: true }));
+let sharedText = "";
 
-app.get("/", (req, res) => {
-  res.send("Привет, это главная страница!");
+// Статические файлы (например, index.html)
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("Пользователь подключился:", socket.id);
+
+  socket.emit("init", sharedText); // Отправляем текущее состояние текста при подключении
+
+  socket.on("text-change", (newtext) => {
+    sharedText = newtext;
+    socket.broadcast.emit("update-text", sharedText);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Пользователь отключился:", socket.id);
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log("Сервер запущен на http://localhost:3000");
 });
